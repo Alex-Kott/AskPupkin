d4636e2e0cf8f38503972b310f1794240a6ef6e8
AlexKott <alexkott@gmail.com>
2017-09-29 18:40:14 +0300

    dcscsdcsdc

--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.template import RequestContext
 
 # Create your views here.
 
 def index(request):
 	return render(request, 'app/index.html')
+
+
+def ask(request):
+	return render(request, 'app/question.html')
