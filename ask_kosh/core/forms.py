81d2a257809ee9fbb6b0f25a94cb14f59a79142e
alexkott <you@example.com>
2017-12-22 16:44:06 +0300

    Intermediate commit

--- ask_kosh/core/forms.py
+++ ask_kosh/core/forms.py
@@ -5,30 +5,36 @@ from ask_kosh.models import Question
 
 
 class SignUpForm(UserCreationForm):
-	first_name = forms.CharField(max_length=30, required=False, help_text='',widget=forms.TextInput(attrs={'class': "form-control"}))
-	last_name = forms.CharField(max_length=30, required=False, help_text='',widget=forms.TextInput(attrs={'class': "form-control"}))
-	email = forms.CharField(max_length=254, help_text='', widget=forms.TextInput(attrs={'class': "form-control"}))
+    first_name = forms.CharField(max_length=30, required=False, help_text='',
+                                 widget=forms.TextInput(attrs={'class': "form-control"}))
+    last_name = forms.CharField(max_length=30, required=False, help_text='',
+                                widget=forms.TextInput(attrs={'class': "form-control"}))
+    email = forms.CharField(max_length=254, help_text='',
+                            widget=forms.TextInput(attrs={'class': "form-control"}))
 
-	class Meta:
-		model = User
-		fields = ['username', 'email', 'first_name', 'first_name', 'password1', 'password2']
-		widgets = {
-			'username': forms.TextInput(attrs={'class': "form-control"}),
-			'password1': forms.PasswordInput(attrs={'class': "form-control"}),
-			'password2': forms.PasswordInput(attrs={'class': "form-control"})
-		}
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'first_name', 'password1', 'password2']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': "form-control"}),
+            'password1': forms.PasswordInput(attrs={'class': "form-control"}),
+            'password2': forms.PasswordInput(attrs={'class': "form-control"})
+        }
 
 
 class SignInForm(forms.Form):
-	username = forms.CharField(max_length=30, required=True, widget=forms.TextInput(attrs={'class': "form-control"}))
-	password = forms.CharField(widget=forms.PasswordInput(attrs={'class': "form-control"}))
+    username = forms.CharField(max_length=30, required=True,
+                               widget=forms.TextInput(attrs={'class': "form-control"}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': "form-control"}))
 
 
 class NewQuestionForm(forms.ModelForm):
-	title = forms.CharField(max_length=150, label="Title", widget=forms.TextInput(attrs={'class': 'form-control'}))
-	text = forms.CharField(widget=forms.Textarea(attrs={'class': "form-control", 'rows': '4', 'cols': '10'}))
-	tags = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    title = forms.CharField(max_length=150, label="Title",
+                            widget=forms.TextInput(attrs={'class': 'form-control'}))
+    text = forms.CharField(
+        widget=forms.Textarea(attrs={'class': "form-control", 'rows': '4', 'cols': '10'}))
+    tags = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
 
-	class Meta:
-		model = Question
-		fields = ['title', 'text', 'tags']
+    class Meta:
+        model = Question
+        fields = ['title', 'text', 'tags']
--- ask_kosh/views.py
+++ ask_kosh/views.py
@@ -4,81 +4,87 @@ from django.contrib.auth.models import User
 from ask_kosh.core.forms import SignUpForm, SignInForm, NewQuestionForm
 from django.contrib.auth import login, authenticate, logout
 
+
 # Create your views here.
 
 
 def index(request):
-	# user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+    # user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
 
-	return render(request, 'ask_kosh/index.html')
+    return render(request, 'ask_kosh/index.html')
 
 
 def ask_kosh(request):
-	form = NewQuestionForm()
+    if request.method == "POST":
+        form = NewQuestionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            title = form.cleaned_data.get('title')
+            text = form.cleaned_data.get('text')
+            tags = form.cleaned_data.get('tags')
 
-	return render(request, 'ask_kosh/ask_kosh.html', {'form': form})
+    else:
+        form = NewQuestionForm()
+
+    return render(request, 'ask_kosh/ask_kosh.html', {'form': form})
 
 
 def question(request, question):
-	return render(request, 'ask_kosh/question.html')
+    return render(request, 'ask_kosh/question.html')
 
 
 def signin(request):
-	if request.method == "POST":
-		form = SignInForm(request.POST)
-		if form.is_valid():
-			username = form.cleaned_data.get('username')
-			password = form.cleaned_data.get('password')
-			user = authenticate(username=username, password=password)
-			if user is not None:
-				if user.is_active:
-					login(request, user)
-					return redirect('index')
-			else:
-				return render(request, 'ask_kosh/signin.html', {'form': form, 'user': user})
-	else:
-		form = SignInForm()
-	return render(request, 'ask_kosh/signin.html', {'form': form})
+    if request.method == "POST":
+        form = SignInForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('index')
+            else:
+                return render(request, 'ask_kosh/signin.html', {'form': form, 'user': user})
+    else:
+        form = SignInForm()
+    return render(request, 'ask_kosh/signin.html', {'form': form})
 
 
 def signup(request):
-	if request.method == "POST":
-		form = SignUpForm(request.POST)
-		if form.is_valid():
-			form.save()
-			username = form.cleaned_data.get('username')
-			email = form.cleaned_data.get('email')
-			password = form.cleaned_data.get('password')
-			# user = User.objects.create_user(username, email, password)
-			user = authenticate(username=username, password=password)
-			login(request, user)
-			return redirect('index')
-	else:
-		form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            # user = User.objects.create_user(username, email, password)
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignUpForm()
 
-	return render(request, 'ask_kosh/signup.html', {'form': form})
+    return render(request, 'ask_kosh/signup.html', {'form': form})
 
 
 def signout(request):
-	logout(request)
-	return redirect('index')
-
+    logout(request)
+    return redirect('index')
 
 
 def hot(request):
-	return render(request, 'ask_kosh/hot.html')
-	
+    return render(request, 'ask_kosh/hot.html')
+
 
 def year(request, n):
-	return render(request, 'ask_kosh/index.html')
+    return render(request, 'ask_kosh/index.html')
 
 
 def tag(request, tag):
-
-	return render(request, 'ask_kosh/tag.html')
-
+    return render(request, 'ask_kosh/tag.html')
 
 # def paginate(query_set, request):
 #
 # 	return object_page, Paginator
-
